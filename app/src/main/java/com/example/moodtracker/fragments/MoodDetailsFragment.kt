f054c3a7b04bf03e5604a7fa6c97e4784f3593fe
kociaki <szymon.nowakowski@cyfroweszoly.pl>
2025-05-20 11:42:44 +0200

    duzo roboty zapomnialem o commitach

--- app/src/main/java/com/example/moodtracker/fragments/MoodDetailsFragment.kt
+++ app/src/main/java/com/example/moodtracker/fragments/MoodDetailsFragment.kt
@@ -5,30 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.moodtracker.MainActivity
 import com.example.moodtracker.R
+import com.example.moodtracker.data.FakeMoodRepository
+import org.w3c.dom.Text
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MoodDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MoodDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var moodDesc: String? = null
+    private var moodFeel: String? = null
+    private var moodType: String? = null
+    private var moodStatements: MutableList<String>? = null
+    private var moodImportance: Boolean? = false
+    private var moodRating: Float = 0.0f
+
+    private val args: MoodDetailsFragmentArgs by navArgs()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,25 +35,57 @@ class MoodDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_mood_details, container, false)
+
+        val moodDescTextView: TextView = view.findViewById(R.id.mood_details_desc)
+        val moodFeelTextView: TextView = view.findViewById(R.id.mood_details_feeling)
+        val moodTypeTextView: TextView = view.findViewById(R.id.mood_details_type)
+        val moodStatementsTextView: TextView = view.findViewById(R.id.mood_details_statements)
+        val moodImportanceTextView: TextView = view.findViewById(R.id.mood_details_importance)
+        val moodRating: RatingBar = view.findViewById(R.id.mood_details_rating)
+        val moodEmojiTextView: TextView = view.findViewById(R.id.mood_details_emoji)
+        val moodButton: Button = view.findViewById(R.id.mood_details_delete)
+        val moodScheme: LinearLayout = view.findViewById(R.id.mood_details)
+
+        val selectedMood = args.mood
+
+        if(activity is MainActivity)
+        {
+            (requireActivity() as MainActivity).setToolbarBackgroundColor(selectedMood.feeling)
+        }
+
+        moodDescTextView.text = selectedMood.description
+        moodFeelTextView.text = selectedMood.feeling
+        moodTypeTextView.text = selectedMood.type
+        moodTypeTextView.text = selectedMood.type
+        moodImportanceTextView.text = if(selectedMood.importance) "To było dla mnie cos waznego" else "To byla blachostka"
+        moodStatementsTextView.text = selectedMood.statements.joinToString(",\n")
+        moodRating.rating = selectedMood.rating
+        moodEmojiTextView.text = when (selectedMood.feeling){
+            "Zadowolony" -> ":-D"
+            "Moze byc" -> ":/"
+            "Smutny" -> ":("
+            else -> ":0"
+        }
+
+        moodScheme.setBackgroundResource(
+            when(selectedMood.feeling){
+                "Zadowolony" -> R.color.black
+                "Moze byc" -> R.color.white
+                "Smutny" -> R.color.white
+                else -> R.color.white
+            }
+        )
+
+        moodButton.setOnClickListener{
+            FakeMoodRepository.removeMood(selectedMood)
+            findNavController().popBackStack()
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MoodDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MoodDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDetach() {
+        super.onDetach()
+        if (activity is MainActivity) {
+            (requireActivity() as MainActivity).resetToolbarBackgroundColor()
+        }
     }
 }
